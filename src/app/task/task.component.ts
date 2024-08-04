e93c2e826b7ee92c2f70c1db9d3b38269752a352
zyarr.innovation <zyarr.innovation@gmail.com>
2024-08-04 20:43:43 +0530

    task with diffferent color

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,5 +1,6 @@
+// task.component.ts
 import { Component, Input } from '@angular/core';
-import { Task } from '../task.model';
+import { Task, TaskState } from '../task.model';
 
 @Component({
   selector: 'app-task',
@@ -8,4 +9,19 @@ import { Task } from '../task.model';
 })
 export class TaskComponent {
   @Input() task!: Task;
+
+  getTaskStateClass(state: TaskState) {
+    switch(state) {
+      case TaskState.NotStarted:
+        return 'not-started';
+      case TaskState.InProgress:
+        return 'in-progress';
+      case TaskState.Completed:
+        return 'completed';
+      case TaskState.Discarded:
+        return 'discarded';
+      default:
+        return '';
+    }
+  }
 }
